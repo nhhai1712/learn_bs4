fe1a903ee1c8a4487398eda7a81dd4252a2c91c1
NguyenHoangHai <hai.nh204963@sis.hust.edu.vn>
2023-10-15 18:55:10 +0700

    crawl real time

--- test.py
+++ test.py
@@ -1,21 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import time
 
-url = "https://coinmarketcap.com/" 
-response = requests.get(url).text
-soup = BeautifulSoup(response, "html.parser")
+while True:
+    url = "https://coinmarketcap.com/"
+    response = requests.get(url).text
+    soup = BeautifulSoup(response, "html.parser")
 
-tbody = soup.tbody
-trs = tbody.contents
-data = {}
-for tr in trs:
-    name = tr.contents[2].p.string
-    price = tr.contents[3].span.string
-    symbol = tr.find(class_="coin-item-symbol").string
-    data[name] = {"symbol": symbol, "price": price}
+    tbody = soup.tbody
+    trs = tbody.contents
+    data = {}
+    for tr in trs[0:2]:
+        name = tr.contents[2].p.string
+        price = tr.contents[3].span.string
+        symbol = tr.find(class_="coin-item-symbol").string
+        data[name] = {"symbol": symbol, "price": price}
 
-# print(data)
-with open("coin_data.json", "w") as json_file:
-    json.dump(data, json_file, indent=4)
+    # Mở tệp JSON và cập nhật dữ liệu
+    with open("coin_data.json", "w") as json_file:
+        json.dump(data, json_file, indent=4)
 
+    print("Update Data!!!")
+    time.sleep(15)
